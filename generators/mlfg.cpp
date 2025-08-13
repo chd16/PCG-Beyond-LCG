94c553729021acaf1bb5550d250654ada63bc04d
chd16 <159042884+chd16@users.noreply.github.com>
2025-08-13 14:00:11 -0400

    Update mlfg.cpp

--- generators/mlfg.cpp
+++ generators/mlfg.cpp
@@ -1,5 +1,5 @@
 /********************************************************************/
-/*               Parallel 32bit generator                           */
+/*               Parallel 64bit generator                           */
 /*               created by christopher draper                      */
 /*               core MLFG based on the implmentatin in SPRNG       */
 /********************************************************************/
